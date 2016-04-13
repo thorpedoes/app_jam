8b9e3796ea7a8dad4efabd27d31368a371cfce6a
thorpedoes <jthorpe@uci.edu>
2016-04-12 22:35:12 -0700

    added CalendarEvent and Event info classes

---
+++ app/src/main/java/EventInfo.java
@@ -0,0 +1,36 @@
+/**
+ * Created by Thorpedoes on 4/12/2016.
+ */
+public class EventInfo {
+    // Date & Time
+    private int day;
+    private int month;
+    private int year;
+    private int hour;
+    private int minutes;
+
+    // Duration
+    private int durHours;
+    private int durMinutes;
+
+    // Constructor
+    public EventInfo(int newDay, int newMonth, int newYear, int startHour, int startMinute, int newDurHours, int newDurMinutes)
+    {
+        day = newDay;
+        month = newMonth;
+        year = newYear;
+        hour = startHour;
+        minutes = startMinute;
+        durHours = newDurHours;
+        durMinutes = newDurMinutes;
+    }
+
+    // Getters
+    public int getDay() { return day; }
+    public int getMonth(){ return month; }
+    public int getYear() { return year; }
+    public int getHour() { return hour; }
+    public int getMinutes() { return minutes; }
+    public int getDurHours() { return durHours; }
+    public int getDurMinutes() { return durMinutes; }
+}
