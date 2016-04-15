5fff0965b79492f8c73d515711b5a1e248c3e804
cnavasca59 <christian.navasca@gmail.com>
2016-04-14 21:38:22 -0700

    Add some lines to the main activity, clean up a bit

--- app/src/main/java/appjam/scheduler/MainActivity.java
+++ app/src/main/java/appjam/scheduler/MainActivity.java
@@ -1,45 +1,54 @@
 package appjam.scheduler;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
 import android.view.View;
 
-import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-
-    private ArrayList<EventInfo> eventList;
-    private EventReader er = new EventReader();
-
+    private ArrayList<CalendarEvent> eventList;
+    private float m_defaultTextSize = 24f;
+    // hold what's selected
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        int numActivities = 0; // temp, later check against ArrayList<Event>().size()
-        if(numActivities == 0)
+        // eventList = reader.getData()
+        eventList = getSampleEventList();
+        if(eventList.size() == 0)
             showEmptyView();
         else
             showListView();
-        // EventReader er = new EventReader();
     }
-
+    // on destroy pass ArrayLis to reader
     private void showEmptyView() {
         setContentView(R.layout.empty_main);
         registerAddButton();
     }
 
     private void showListView() {
-        // load/fill events
-        ArrayAdapter<EventInfo> adapter = new ArrayAdapter<>(this, R.layout.content_main, eventList);
+        // load the events
         setContentView(R.layout.content_main);
+        setDateText();
         registerAddButton();
-        adapter.notifyDataSetChanged();
+        updateTableLayout();
+    }
+
+    private void setDateText() {
+        TextView title = (TextView) findViewById(R.id.dateText);
+        title.setText(currentDateString());
     }
 
     private void registerAddButton() {
@@ -53,5 +62,65 @@ public class MainActivity extends AppCompatActivity {
                 }
             });
         }
+    }
+
+    private void updateTableLayout() {
+        TableLayout tl = (TableLayout) findViewById(R.id.tableLayout);
+        assert tl != null;
+
+        tl.addView(getDivider());
+        for(CalendarEvent ce : eventList) {
+            TableRow row = new TableRow(this);
+            TextView timeCol = getDefaultRowText();
+            TextView tv = getDefaultRowText();
+            tv.setText(ce.getTitle());
+            timeCol.setText(String.format("%d:00PM", ce.getEventInfo().getHour()));
+
+            LinearLayout ll = new LinearLayout(row.getContext());
+            ll.setOrientation(LinearLayout.HORIZONTAL);
+
+            ll.addView(timeCol);
+            ll.addView(tv);
+
+            row.addView(ll);
+            row.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
+            tl.addView(row);
+            tl.addView(getDivider());
+        }
+    }
+
+    private ArrayList<CalendarEvent> getSampleEventList() {
+        ArrayList<CalendarEvent> result = new ArrayList<>();
+        EventInfo testInfo = new EventInfo(1,2,3,1,2,0,2);
+        CalendarEvent testCE = new CalendarEvent("Test", "Something", testInfo, false, 0, "cool music");
+        CalendarEvent testCE1 = new CalendarEvent("Test", "SomethingElse", testInfo, false, 0, "cool music");
+        result.add(testCE);
+        result.add(testCE1);
+        return result;
+    }
+
+    private String currentDateString() {
+        return "Thursday, April 14th"; // obviously don't leave this hard coded
+    }
+
+    private int findRowPadding() {
+        int paddingPixel = 25;
+        float density = getResources().getDisplayMetrics().density;
+        return (int)(paddingPixel * density);
+    }
+
+    private ImageView getDivider() {
+        ImageView divider = new ImageView(this);
+        divider.setLayoutParams(new TableLayout.LayoutParams(TableLayout.LayoutParams.MATCH_PARENT, 1));
+        divider.setBackgroundColor(Color.BLACK);
+        return divider;
+    }
+
+    private TextView getDefaultRowText() {
+        TextView result = new TextView(this);
+        int paddingDp = findRowPadding();
+        result.setPadding(0, 0, paddingDp, 0);
+        result.setTextSize(m_defaultTextSize);
+        return result;
     }
 }
