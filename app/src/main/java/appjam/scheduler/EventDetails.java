2e588988a233f33bafadad2e46cf4495d71c3dc4
thorpedoes <jthorpe@uci.edu>
2016-04-19 20:43:53 -0700

    Can select Date and times from the Add event page

--- app/src/main/java/appjam/scheduler/EventDetails.java
+++ app/src/main/java/appjam/scheduler/EventDetails.java
@@ -1,11 +1,14 @@
 package appjam.scheduler;
 
+import android.app.TimePickerDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 public class EventDetails extends AppCompatActivity {
@@ -13,12 +16,16 @@ public class EventDetails extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_details);
-        registerButtons();
+        registerButtons(savedInstanceState);
     }
 
-    private void registerButtons() {
+    private void registerButtons(Bundle savedInstanceState) {
         Button cancelButton = (Button) findViewById(R.id.cancelButton);
         Button saveButton = (Button) findViewById(R.id.saveButton);
+        Button startDateButton = (Button) findViewById(R.id.startDateButton);
+        Button startTimeButton = (Button) findViewById(R.id.startTimeButton);
+        Button endDateButton = (Button) findViewById(R.id.endDateButton);
+        Button endTimeButton = (Button) findViewById(R.id.endTimeButton);
         if(cancelButton != null)
             cancelButton.setOnClickListener(getExitListener());
         if(saveButton != null)
@@ -39,12 +46,22 @@ public class EventDetails extends AppCompatActivity {
             public void onClick(View v) {
                 if(validEventEntered()) {
                     Intent resultData = new Intent();
-                    resultData.putExtra("newEvent", getEnteredEvent());
+                    resultData.putExtra("New event", getEnteredEvent());
                     setResult(RESULT_OK, resultData);
                     finish();
                 }
             }
         };
+    }
+
+    public void showTimePickerDialog(View v) {
+        DialogFragment newFrag = new TimePickerFragment();
+        newFrag.show(getSupportFragmentManager(), "timePicker");
+    }
+
+    public void showDatePickerDialog(View v) {
+        DialogFragment newFrag = new DatePickerFragment();
+        newFrag.show(getSupportFragmentManager(), "datePicker");
     }
 
     private boolean validEventEntered() {
