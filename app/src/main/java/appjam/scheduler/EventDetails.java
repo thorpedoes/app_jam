7d7cb7a208afe458d7ef4286378ddaa07e85f460
thorpedoes <jthorpe@uci.edu>
2016-04-19 23:46:26 -0700

    select time and date set to current date

--- app/src/main/java/appjam/scheduler/EventDetails.java
+++ app/src/main/java/appjam/scheduler/EventDetails.java
@@ -11,6 +11,8 @@ import android.widget.Button;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
+import java.util.Calendar;
+
 public class EventDetails extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,14 +24,43 @@ public class EventDetails extends AppCompatActivity {
     private void registerButtons(Bundle savedInstanceState) {
         Button cancelButton = (Button) findViewById(R.id.cancelButton);
         Button saveButton = (Button) findViewById(R.id.saveButton);
+
+        Calendar cal = Calendar.getInstance();
         Button startDateButton = (Button) findViewById(R.id.startDateButton);
+        startDateButton.setText(calendarDate(cal));
         Button startTimeButton = (Button) findViewById(R.id.startTimeButton);
+        startTimeButton.setText(calendarTime(cal));
         Button endDateButton = (Button) findViewById(R.id.endDateButton);
+        endDateButton.setText(calendarDate(cal));
         Button endTimeButton = (Button) findViewById(R.id.endTimeButton);
+        endTimeButton.setText(calendarTime(cal));
         if(cancelButton != null)
             cancelButton.setOnClickListener(getExitListener());
         if(saveButton != null)
             saveButton.setOnClickListener(getSaveListener());
+    }
+
+    private String calendarDate(Calendar cal) {
+        int month, day, year;
+        String dateString = null;
+
+        month = cal.get(Calendar.MONTH);
+        day = cal.get(Calendar.DAY_OF_MONTH);
+        year = cal.get(Calendar.YEAR);
+        dateString = Integer.toString(month) + "/" + Integer.toString(day) + "/" + Integer.toString(year);
+
+        return dateString;
+    }
+
+    private String calendarTime(Calendar cal) {
+        int hour, minute;
+        String timeString = null;
+
+        hour = cal.get(Calendar.HOUR_OF_DAY);
+        minute = cal.get(Calendar.MINUTE);
+        timeString = Integer.toString(hour) + ":" + Integer.toString(minute);
+
+        return timeString;
     }
 
     private View.OnClickListener getExitListener() {
@@ -54,14 +85,14 @@ public class EventDetails extends AppCompatActivity {
         };
     }
 
-    public void showTimePickerDialog(View v) {
-        DialogFragment newFrag = new TimePickerFragment();
-        newFrag.show(getSupportFragmentManager(), "timePicker");
-    }
-
     public void showDatePickerDialog(View v) {
         DialogFragment newFrag = new DatePickerFragment();
         newFrag.show(getSupportFragmentManager(), "datePicker");
+    }
+
+    public void showTimePickerDialog(View v) {
+        DialogFragment newFrag = new TimePickerFragment();
+        newFrag.show(getSupportFragmentManager(), "timePicker");
     }
 
     private boolean validEventEntered() {
