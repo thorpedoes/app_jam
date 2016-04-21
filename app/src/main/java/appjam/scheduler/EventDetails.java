38b879e867bdf66672334c8742671a6714765386
cnavasca59 <christian.navasca@gmail.com>
2016-04-20 19:04:23 -0700

    Connect button to time string

--- app/src/main/java/appjam/scheduler/EventDetails.java
+++ app/src/main/java/appjam/scheduler/EventDetails.java
@@ -4,11 +4,13 @@ import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.app.TimePickerDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.format.DateFormat;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
@@ -18,6 +20,11 @@ import android.widget.Toast;
 import java.util.Calendar;
 
 public class EventDetails extends AppCompatActivity {
+
+    private Button startDateButton;// = (Button) findViewById(R.id.startDateButton);
+    private Button endDateButton;// = (Button) findViewById(R.id.endDateButton);
+    private Button startTimeButton;// = (Button) findViewById(R.id.startTimeButton);
+    private Button endTimeButton;// = (Button) findViewById(R.id.endTimeButton);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,14 +38,19 @@ public class EventDetails extends AppCompatActivity {
         Button saveButton = (Button) findViewById(R.id.saveButton);
 
         Calendar cal = Calendar.getInstance();
-        Button startDateButton = (Button) findViewById(R.id.startDateButton);
+
+        startDateButton = (Button) findViewById(R.id.startDateButton);
         startDateButton.setText(calendarDate(cal));
-        Button startTimeButton = (Button) findViewById(R.id.startTimeButton);
+
+        startTimeButton = (Button) findViewById(R.id.startTimeButton);
         startTimeButton.setText(calendarTime(cal));
-        Button endDateButton = (Button) findViewById(R.id.endDateButton);
+
+        endDateButton = (Button) findViewById(R.id.endDateButton);
         endDateButton.setText(calendarDate(cal));
-        Button endTimeButton = (Button) findViewById(R.id.endTimeButton);
+
+        endTimeButton = (Button) findViewById(R.id.endTimeButton);
         endTimeButton.setText(calendarTime(cal));
+
         if(cancelButton != null)
             cancelButton.setOnClickListener(getExitListener());
         if(saveButton != null)
@@ -95,6 +107,7 @@ public class EventDetails extends AppCompatActivity {
     public void showDatePickerDialog(View v) {
         DialogFragment newFrag = new DatePickerFragment();
         newFrag.show(getSupportFragmentManager(), "datePicker");
+        //Button sdb = (Button) newFrag.getActivity().findViewById(R.id.startDateButton);
     }
 
     public void showTimePickerDialog(View v) {
@@ -116,6 +129,7 @@ public class EventDetails extends AppCompatActivity {
     private CalendarEvent getEnteredEvent() {
         CalendarEvent result = new CalendarEvent();
         // get and set all fields
+        result.setStartTime(TimePickerFragment.getStart());
         return result;
     }
 
@@ -145,9 +159,11 @@ public class EventDetails extends AppCompatActivity {
 
     public static class TimePickerFragment extends DialogFragment
             implements TimePickerDialog.OnTimeSetListener {
+        private static Calendar c = Calendar.getInstance();
+        private static Calendar m_selectedStartTime = Calendar.getInstance();
+
         @Override
         public Dialog onCreateDialog(Bundle savedInstance) {
-            final Calendar c = Calendar.getInstance();
             int hour = c.get(Calendar.HOUR_OF_DAY);
             int minute = c.get(Calendar.MINUTE);
 
@@ -155,8 +171,44 @@ public class EventDetails extends AppCompatActivity {
                     DateFormat.is24HourFormat(getActivity()));
         }
 
-        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        @Override
+        public void onDismiss(DialogInterface dialog) {
+            Button startTimeButton = (Button) this.getActivity().findViewById(R.id.startTimeButton);
+            startTimeButton.setText(calendarTime(m_selectedStartTime));
+        }
 
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            m_selectedStartTime.set(Calendar.HOUR_OF_DAY, hourOfDay);
+            m_selectedStartTime.set(Calendar.MINUTE, minute);
+        }
+
+        public static Calendar getStart() {
+            return m_selectedStartTime;
+        }
+
+        private String calendarDate(Calendar cal) {
+            int month, day, year;
+            String dateString = null;
+
+            month = cal.get(Calendar.MONTH);
+            day = cal.get(Calendar.DAY_OF_MONTH);
+            year = cal.get(Calendar.YEAR);
+            dateString = Integer.toString(month + 1) + "/" + Integer.toString(day) + "/" + Integer.toString(year);
+
+            return dateString;
+        }
+
+        private String calendarTime(Calendar cal) {
+            int hour, minute;
+            String timeString = null;
+
+            hour = cal.get(Calendar.HOUR_OF_DAY);
+            minute = cal.get(Calendar.MINUTE);
+            if (hour == 0) timeString = Integer.toString(hour + 12) + ":" + Integer.toString(minute) + " AM";
+            else if (hour >= 12) timeString = Integer.toString(hour - 11) + ":" + Integer.toString(minute) + " PM";
+            else timeString = Integer.toString(hour) + ":" + Integer.toString(minute) + " AM";
+
+            return timeString;
         }
     }
 
